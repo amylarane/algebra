387419a97a55a49ba18e7a730282c69dcc959987
Amy Larane <amylarane@gmail.com>
2021-01-22 22:00:12 -0600

    Rewrote the parser to be simpler

--- src/parse.rs
+++ src/parse.rs
@@ -4,14 +4,7 @@ pub enum Number {
     Variable(String),
 }
 
-#[derive(Debug)]
-pub enum Expression {
-    Number(Number),
-    Operation(Operation, Box<Expression>, Box<Expression>),
-    Unary(Operation, Box<Expression>),
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Operation {
     Addition,
     Subtraction,
@@ -21,184 +14,156 @@ pub enum Operation {
 }
 
 #[derive(Debug)]
+pub enum Expression {
+    Number(Number),
+    Operation(Operation, Box<Expression>, Box<Expression>),
+    Unary(Operation, Box<Expression>),
+}
+
+#[derive(Debug)]
 pub struct Statement {
     left: Expression,
     right: Expression,
 }
 
-pub fn parse_statement(math: String) -> Statement {
-    let (left, math) = parse_expression(math.trim().chars().collect());
+fn get_first(s: &String) -> Option<char> {
+    s.trim().chars().next()
+}
 
-    if math.trim().chars().next().unwrap() != '=' {
-        panic!("Exprected '='");
+fn get_rest(s: &String) -> Option<String> {
+    match s.len() {
+        0 => None,
+        _ => Some(
+            s.trim()
+                .chars()
+                .skip(1)
+                .collect::<String>()
+                .trim()
+                .chars()
+                .collect(),
+        ),
     }
+}
 
-    let (right, math) = parse_expression(
-        math.trim()
-            .chars()
-            .skip(1)
-            .collect::<String>()
-            .trim()
-            .chars()
-            .collect(),
-    );
+fn pull(s: &String) -> (Option<char>, Option<String>) {
+    (get_first(s), get_rest(s))
+}
 
-    if math.trim().len() != 0 {
-        panic!("Expected end of string");
+pub fn parse_statement(math: String) -> Statement {
+    let (left, math) = parse_expression(math);
+
+    let (right, math) = match pull(&math) {
+        (Some(first), Some(rest)) => match first {
+            '=' => parse_expression(rest),
+            c => panic!("Expected '=' found {}", c),
+        },
+        (Some(first), None) => match first {
+            '=' => panic!("Expected right side of equation, got end of string"),
+            _ => panic!("Got unexpected character"),
+        },
+        _ => panic!("unexpected end of string"),
+    };
+
+    if math.len() != 0 {
+        panic!("Expected end of string")
     }
 
     Statement { left, right }
 }
 
-fn get_first(s: &String) -> char {
-    s.trim().chars().next().unwrap()
+use std::collections::HashMap;
+pub struct OpTable {
+    operations: HashMap<char, Operation>,
+    next_level: Option<Box<OpTable>>,
 }
 
-fn get_rest(s: &String) -> String {
-    s.trim()
-        .chars()
-        .skip(1)
-        .collect::<String>()
-        .trim()
-        .chars()
-        .collect()
-}
+pub fn parse_binary(math: String, operations: &OpTable) -> (Expression, String) {
+    match operations.next_level {
+        Some(ref table) => {
+            let (left, math) = parse_binary(math, table);
 
-fn pull(s: &String) -> (char, String) {
-    (get_first(s), get_rest(s))
+            match pull(&math) {
+                (Some(first), Some(rest)) if operations.operations.contains_key(&first) => {
+                    let (right, math) = parse_binary(rest, operations);
+                    (
+                        Expression::Operation(
+                            operations.operations[&first].clone(),
+                            box left,
+                            box right,
+                        ),
+                        math,
+                    )
+                }
+                _ => (left, math),
+            }
+        }
+        None => parse_unary(math),
+    }
 }
 
 pub fn parse_expression(math: String) -> (Expression, String) {
-    parse_addition(math)
-}
-
-pub fn parse_addition(math: String) -> (Expression, String) {
-    let (left, math) = parse_multiplication(math);
-
-    if math.len() > 0 {
-        let (first, rest) = pull(&math);
-
-        match first {
-            '+' | '-' => {
-                let (right, math) = parse_addition(rest);
-                (
-                    Expression::Operation(
-                        match first {
-                            '+' => Operation::Addition,
-                            '-' => Operation::Subtraction,
-                            _ => panic!("Excuse me WTF"),
-                        },
-                        box left,
-                        box right,
-                    ),
-                    math,
-                )
-            }
-            _ => (left, math),
-        }
-    } else {
-        (left, math)
-    }
-}
-
-pub fn parse_multiplication(math: String) -> (Expression, String) {
-    let (left, math) = parse_exponentiation(math);
-
-    if math.len() > 0 {
-        let (first, rest) = pull(&math);
-
-        match first {
-            '*' | '/' => {
-                let (right, math) = parse_multiplication(rest);
-                (
-                    Expression::Operation(
-                        match first {
-                            '*' => Operation::Multiplication,
-                            '/' => Operation::Division,
-                            _ => panic!("Excuse me WTF"),
-                        },
-                        box left,
-                        box right,
-                    ),
-                    math,
-                )
-            }
-            _ => (left, math),
-        }
-    } else {
-        (left, math)
-    }
-}
-
-pub fn parse_exponentiation(math: String) -> (Expression, String) {
-    let (left, math) = parse_unary(math);
-
-    if math.len() > 0 {
-        let (first, rest) = pull(&math);
-
-        match first {
-            '^' => {
-                let (right, math) = parse_exponentiation(rest);
-                (
-                    Expression::Operation(
-                        match first {
-                            '^' => Operation::Exponentiation,
-                            _ => panic!("Excuse me WTF"),
-                        },
-                        box left,
-                        box right,
-                    ),
-                    math,
-                )
-            }
-            _ => (left, math),
-        }
-    } else {
-        (left, math)
-    }
+    parse_binary(
+        math,
+        &OpTable {
+            operations: [('+', Operation::Addition), ('-', Operation::Subtraction)]
+                .iter()
+                .cloned()
+                .collect(),
+            next_level: Some(box OpTable {
+                operations: [('*', Operation::Multiplication), ('/', Operation::Division)]
+                    .iter()
+                    .cloned()
+                    .collect(),
+                next_level: Some(box OpTable {
+                    operations: [('^', Operation::Exponentiation)].iter().cloned().collect(),
+                    next_level: None,
+                }),
+            }),
+        },
+    )
 }
 
 pub fn parse_unary(math: String) -> (Expression, String) {
-    if math.len() > 0 {
-        let (first, rest) = pull(&math);
-
-        match first {
-            '+' | '-' => {
-                let (unary, math) = parse_unary(rest);
-                (
-                    Expression::Unary(
-                        match first {
-                            '+' => Operation::Addition,
-                            '-' => Operation::Subtraction,
-                            _ => panic!("WTF"),
-                        },
-                        box unary,
-                    ),
-                    math,
-                )
-            }
-            _ => parse_low(math),
+    match pull(&math) {
+        (Some(first), Some(rest)) if first == '+' || first == '-' => {
+            let (operation, math) = parse_unary(rest);
+            (
+                Expression::Unary(
+                    match first {
+                        '+' => Operation::Addition,
+                        '-' => Operation::Subtraction,
+                        _ => panic!("Unexpected operator you broke something"),
+                    },
+                    box operation,
+                ),
+                math,
+            )
         }
-    } else {
-        panic!("Error, need more math")
+        (Some(_), Some(_)) => parse_low(math),
+        _ => panic!("Error, expected rest of expression got end of string"),
     }
 }
 
 pub fn parse_low(math: String) -> (Expression, String) {
-    let (first, rest) = pull(&math);
-
-    match first {
-        x if x.is_digit(10) => parse_num(math),
-        x if x.is_alphabetic() => (Expression::Number(Number::Variable(x.to_string())), rest),
-        '(' => {
+    match pull(&math) {
+        (Some('('), Some(rest)) => {
             let (expr, math) = parse_expression(rest);
-            let (first, math) = pull(&math);
-            if first != ')' {
-                panic!("Error, expected ')'");
+            match pull(&math) {
+                (Some(')'), Some(rest)) => (expr, rest),
+                (Some(first), _) => panic!("Expected ')' got {}", first),
+                _ => panic!("Expected ')' got end of string"),
             }
-            (expr, math)
         }
-        _ => panic!("Error"),
+        (Some(first), _) if first.is_digit(10) => parse_num(math),
+        (Some(first), rest) if first.is_alphabetic() => (
+            Expression::Number(Number::Variable(first.to_string())),
+            match rest {
+                Some(string) => string,
+                None => "".to_string(),
+            },
+        ),
+        (Some(first), Some(_)) => panic!("Unexpected character {}", first),
+        (_, _) => panic!("Unexpected end of string"),
     }
 }
 
